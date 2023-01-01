7ae302907c300b6a314011d8bbd3704be7ca75ef
liang.zhihao <liang13268932888@outlook.com>
2023-01-01 11:39:31 +0800

    feat: support book gallery

--- lib/heatmap.dart
+++ lib/heatmap.dart
@@ -13,7 +13,7 @@ class MyHeatMap extends StatefulWidget {
 }
 
 class _MyHeatMapState extends State<MyHeatMap> {
-  String dataBaseFile = "assets/data/db.json";
+  String databaseFile = "assets/data/db.json";
 
   @override
   void dispose() {
@@ -23,17 +23,18 @@ class _MyHeatMapState extends State<MyHeatMap> {
   @override
   Widget build(BuildContext context) {
     // read json file
-    String jsonString = File(dataBaseFile).readAsStringSync();
+    String jsonString = File(databaseFile).readAsStringSync();
     // parse tokens
-    Map<String, dynamic> dataBase = jsonDecode(jsonString)["tokens"];
-    int runningCount = 0, readingCount = 0;
+    Map<String, dynamic> tokenDatabase = jsonDecode(jsonString)["tokens"];
+    Map<String, dynamic> bookDatabase = jsonDecode(jsonString)["books"];
+    int runningCount = 0, paperCount = 0;
     Map<DateTime, int> runningMapDataset = {};
     Map<DateTime, int> readingMapDataset = {};
-    dataBase.forEach((date, value) {
+    tokenDatabase.forEach((date, value) {
       int running = value["running"] ?? 0;
       List<String> reading = (value["reading"] as List<dynamic>).cast<String>();
       runningCount += running;
-      readingCount += reading.length;
+      paperCount += reading.length;
       if (running > 0) {
         runningMapDataset[DateTime.parse(date)] = running;
       }
@@ -42,19 +43,31 @@ class _MyHeatMapState extends State<MyHeatMap> {
       }
     });
 
+    // parse books
+    int bookCount = 0;
+
+    for (String category in bookDatabase.keys) {
+      List<String> bookList =
+          (bookDatabase[category] as List<dynamic>).cast<String>();
+      bookCount += bookList.length;
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromRGBO(94, 161, 254, 1.0),
-        title: const Text("My Calendar Heatmap"),
+        title: const Text("Calendar Heatmap && Book Gallary"),
       ),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              " 🏃‍累计跑步（公里）：${runningCount}",
-              textAlign: TextAlign.left,
-              textScaleFactor: 2,
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                " 🏃‍累计跑步（公里）：${runningCount}",
+                textAlign: TextAlign.left,
+                textScaleFactor: 1.6,
+              ),
             ),
             Card(
               margin: const EdgeInsets.all(10),
@@ -62,8 +75,11 @@ class _MyHeatMapState extends State<MyHeatMap> {
               child: Padding(
                 padding: const EdgeInsets.all(10),
                 child: HeatMap(
+                  borderRadius: 2,
+                  margin: const EdgeInsets.all(1.2),
+                  size: 12,
+                  fontSize: 10,
                   datasets: runningMapDataset,
-                  size: 18,
                   scrollable: true,
                   startDate: DateTime(2023, 1, 1),
                   endDate: DateTime(2023, 12, 31),
@@ -75,10 +91,13 @@ class _MyHeatMapState extends State<MyHeatMap> {
                 ),
               ),
             ),
-            Text(
-              " 📰累计阅读论文：${readingCount}",
-              textAlign: TextAlign.left,
-              textScaleFactor: 2,
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                " 📰累计阅读论文：${paperCount}",
+                textAlign: TextAlign.left,
+                textScaleFactor: 1.6,
+              ),
             ),
             Card(
               margin: const EdgeInsets.all(10),
@@ -86,8 +105,11 @@ class _MyHeatMapState extends State<MyHeatMap> {
               child: Padding(
                 padding: const EdgeInsets.all(10),
                 child: HeatMap(
+                  borderRadius: 2,
+                  margin: const EdgeInsets.all(1.2),
+                  size: 12,
+                  fontSize: 10,
                   datasets: readingMapDataset,
-                  size: 18,
                   scrollable: true,
                   startDate: DateTime(2023, 1, 1),
                   endDate: DateTime(2023, 12, 31),
@@ -97,6 +119,96 @@ class _MyHeatMapState extends State<MyHeatMap> {
                     1: Color.fromRGBO(218, 65, 64, 1.0),
                   },
                 ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                " 📘累计阅读书籍：${bookCount}",
+                textAlign: TextAlign.left,
+                textScaleFactor: 1.6,
+              ),
+            ),
+            SizedBox(
+              height: 300,
+              child: ListView.builder(
+                itemCount: bookDatabase.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(bookDatabase.keys.toList()[index]),
+                      ),
+                      GridView.builder(
+                        shrinkWrap: true,
+                        itemCount: bookDatabase.values.toList()[index].length,
+                        physics: const NeverScrollableScrollPhysics(),
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 6,
+                          mainAxisSpacing: 10,
+                          crossAxisSpacing: 10,
+                          childAspectRatio: 0.7,
+                        ),
+                        itemBuilder: (BuildContext context, int bookIndex) {
+                          return Column(
+                            children: <Widget>[
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                child: FadeInImage.assetNetwork(
+                                  height: 130,
+                                  fit: BoxFit.fill,
+                                  placeholder:
+                                      "assets/images/bookPlaceholder.png",
+                                  image: bookDatabase.values.toList()[index]
+                                      [bookIndex]["file"],
+                                  imageErrorBuilder:
+                                      ((context, error, stackTrace) {
+                                    return Image.asset(
+                                        height: 130,
+                                        fit: BoxFit.fill,
+                                        "assets/images/bookPlaceholder.png");
+                                  }),
+                                ),
+                              ),
+                              // Card(
+                              //   child: FadeInImage.assetNetwork(
+                              //     fit: BoxFit.cover,
+                              //     placeholder:
+                              //         "assets/images/bookPlaceholder.png",
+                              //     image: bookDatabase.values.toList()[index]
+                              //         [bookIndex]["file"],
+                              //     imageErrorBuilder:
+                              //         ((context, error, stackTrace) {
+                              //       return Image.asset(
+                              //           fit: BoxFit.fill,
+                              //           "assets/images/bookPlaceholder.png");
+                              //     }),
+                              //   ),
+                              // ),
+                              SizedBox(
+                                width: 80,
+                                child: Text(
+                                  bookDatabase.values.toList()[index][bookIndex]
+                                      ["title"],
+                                  softWrap: true,
+                                  textAlign: TextAlign.left,
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 1,
+                                  style: const TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              )
+                            ],
+                          );
+                        },
+                      )
+                    ],
+                  );
+                },
               ),
             ),
           ],
